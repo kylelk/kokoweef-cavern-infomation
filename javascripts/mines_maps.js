27714f512774387a4a1f7e99e1bda649adb8b5bd
kyle kersey <kylelk@comcast.net>
2014-08-09 11:56:31 -0700

    fixed maps

--- javascripts/mines_maps.js
+++ javascripts/mines_maps.js
@@ -1,17 +1,14 @@
 function initialize() {
-  var chicago = new google.maps.LatLng(35.420347, -115.493836);
+  var kokoweef_cavern = new google.maps.LatLng(35.420347, -115.493836);
   var mapOptions = {
     zoom: 11,
-    center: chicago,
+    center: kokoweef_cavern,
     mapTypeId: google.maps.MapTypeId.SATELLITE
   }
 
   var map = new google.maps.Map(document.getElementById('nearby_mines_map'), mapOptions);
-
-  var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://kylelk.github.io/kokoweef-cavern-infomation/mines.kmz'
-  });
-  ctaLayer.setMap(map);
+  var kmzLayer = new google.maps.KmlLayer('http://kylelk.github.io/kokoweef-cavern-infomation/mines.kmz');
+  kmzLayer.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
